cdd5c51a68c0517f6458be71ea97968eb3244c35
johwon <wwalxxn@naver.com>
2025-05-26 01:25:30 +0900

    feat: [001-ADD-USER] 유저 생성 기능 구현

---
+++ src/main/java/com/example/jpa/user/controller/ApiUserController.java
@@ -0,0 +1,69 @@
+package com.example.jpa.user.controller;
+
+import com.example.jpa.notice.model.ResponseError;
+import com.example.jpa.user.entity.User;
+import com.example.jpa.user.model.UserInput;
+import com.example.jpa.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class ApiUserController {
+
+    private final UserRepository userRepository;
+
+//    @PostMapping
+//    public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
+//
+//        List<ResponseError> responseErrorList = new ArrayList<>();
+//
+//        if(errors.hasErrors()){
+//            errors.getAllErrors().forEach((e)->{
+//                responseErrorList.add(ResponseError.of((FieldError) e));
+//            });
+//
+//            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+//        }
+//
+//        return ResponseEntity.ok().build();
+//    }
+
+    @PostMapping
+    public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
+
+        List<ResponseError> responseErrorList = new ArrayList<>();
+
+        if(errors.hasErrors()){
+            errors.getAllErrors().forEach((e)->{
+                responseErrorList.add(ResponseError.of((FieldError) e));
+            });
+
+            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+        }
+
+        User user = User.builder()
+                .email(userInput.getEmail())
+                .username(userInput.getUsername())
+                .password(userInput.getPassword())
+                .phone(userInput.getPhone())
+                .regDate(LocalDateTime.now())
+                .build();
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
+
+
+}
